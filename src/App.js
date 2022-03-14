542ab875188215ec1bf4b79900cc07bf1e2c0819
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2022-03-14 12:38:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      This is first app
+      <ExpenseItem
+        t="Expense 1"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem(props) {
+    return <h2>Expense Item - {props.t}</h2>
+}
+
+export default ExpenseItem;
